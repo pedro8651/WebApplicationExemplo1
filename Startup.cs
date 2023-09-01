2c7f92e60b4b8c8a024d146551772745cb7b075c
Luis Oliveira <luis11003156@edu.pa.senac.br>
2023-09-01 17:51:14 -0300

    Adicionar arquivos de projeto.

---
+++ Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationExemplo1
+{
+    public class Startup{
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var _mySQLServerVersion = new MySqlServerVersion(new Version(8, 0, 3));
+
+            services.AddDbContext<ApplicationDbContext>(
+                                    options => options.UseMySql(
+                                                        Configuration.GetConnectionString("ApplicationDbContext"),
+                                                        _mySQLServerVersion
+                                                    )
+                                    );
+
+            services.AddRazorPages();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
